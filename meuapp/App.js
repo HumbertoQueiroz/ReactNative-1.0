f4e932f4c3833a4fdb3643da2724d0890b2c0fea
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2025-03-18 11:31:19 -0400

    add

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,19 +1,66 @@
 import React, { Component } from "react";
-import { View, Text, Image, Button, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Image, Button, TextInput, StyleSheet, TouchableOpacity, Alert } from "react-native";
 
 class App extends Component{
 
+	constructor(props){
+		super(props);
+		this.state={
+			textoFrase:"",
+			img:require('./src/logo_mini.png'),
+			isBigImg:false
+		}
+		this.exibeFrase=this.exibeFrase.bind(this)
+		this.frases=[
+			"Que a força esteja com você. - Star Wars",
+			"A vida encontra um caminho. - Jurassic Park",
+			"Até a vitória, sempre. - Capitão América: O Primeiro Vingador",
+			"Por que caímos? Para aprendermos a nos levantar. - Batman Begins",
+			"Grandes poderes trazem grandes responsabilidades. - Homem-Aranha",
+			"A esperança é uma coisa boa, talvez a melhor de todas, e coisas boas nunca morrem. - Um Sonho de Liberdade",
+			"Oh sim, o passado pode doer. Mas, do jeito que vejo, você pode fugir dele ou aprender com ele. - O Rei Leão",
+			"O que fazemos em vida ecoa na eternidade. - Gladiador",
+			"Alguns são feitos para usar as asas, outros são feitos para usar as muletas. - Um Sonho de Liberdade",
+			"Nós vamos precisar de um barco maior. - Tubarão",
+			"Ao infinito... e além! - Toy Story",
+			"A vida não é sobre o quão duro você consegue bater, mas sobre quão duro você consegue ser atingido e continuar seguindo em frente. É assim que a vitória é conquistada! - Rocky Balboa",
+			"A única maneira de lidar com o mundo é abraçar sua loucura. - O Lado Bom da Vida",
+			"Não importa o que digam a você, palavras e ideias podem mudar o mundo. - Sociedade dos Poetas Mortos",
+			"A aventura é lá fora! - Up: Altas Aventuras",
+			"O que você espera? Que eu diga 'tudo bem'? Que tudo fique bem? O mundo não é assim, como você conta para seus filhos que ele é. - O Curioso Caso de Benjamin Button",
+			"A única maneira de vencer é jogar. - Xadrez",
+			"Não tente. Faça ou não faça. Não existe tentar. - Star Wars: Episódio V - O Império Contra-Ataca",
+			"A jornada é mais importante que o destino. - Piratas do Caribe: A Maldição do Pérola Negra",
+			"Acredite em si mesmo e tudo será possível. - Ratatouille"
+		]
+	}
+
+	exibeFrase(){
+		const numeroAleatorio= Math.floor(Math.random() * this.frases.length);
+		this.setState((prevState) => ({
+      textoFrase: `"${this.frases[numeroAleatorio]}"`,
+      isBigImg: !prevState.isBigImg,
+      img: prevState.isBigImg
+        ? require("./src/logo_mini.png")
+        : require("./src/logo_long.png"),
+    }));
+  
+	}
+
 	render(){
 		return(
-			<View style={styles.containerMain}>
+			<View style={[styles.containerMain,styles.alignCenter]}>
 				<Image 
-					source={require('./src/logo_mini.png')}
-					style={styles.imgSmall}
+					source={this.state.img}
+					style={this.state.isBigImg?styles.imgBig:styles.imgSmall}
 				/>
-				<Text>"Alguma Frase Aqui"</Text>
-				<TouchableOpacity>
-					<View>
-						<Text>Exibir Logo completa </Text>
+				<Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+				<TouchableOpacity 
+					style={styles.button}
+					onPress={this.exibeFrase}
+				>
+					<View style={styles.btnArea}>
+						<Text style={styles.btnTexto}>Surpreender-se </Text>
 					</View>
 				</TouchableOpacity>
 			</View>
@@ -170,9 +217,41 @@ const styles = StyleSheet.create({
 		backgroundColor: "#f1f1f1f1",
 		paddingTop:'13%'
 	},
+	alignCenter:{
+		alignItems:'center',
+		justifyContent:'center'
+	},
+	textoFrase:{
+		fontSize:20,
+		color:'#dd7b22',
+		margin:30,
+		textAlign:'center'
+	},
+	button:{
+		width:230,
+		height:50,
+		borderWidth:2,
+		borderColor:'#dd7b22',
+		borderRadius:50,
+	},
+	btnArea:{
+		flex:1,
+		flexDirection:'',
+		justifyContent:'center',
+		alignItems:'center'
+		},
+		btnTexto:{
+			fontSize:18,
+			fontWeight:'bold',
+			color:'#dd7b22'
+		},
 	imgSmall:{
-		width:393,
-		height:290
+		width:67.7,
+		height:50
+	},
+	imgBig:{
+		width:400,
+		height:74.3
 	},
 	input: {
 		height: 45,
